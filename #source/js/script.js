d12ce2bde6341504bb873b35e4f61c98e5e5ac56
hron2017 <andreypbiz@gmail.com>
2020-11-04 21:13:18 +0300

    first commit

--- #source/js/script.js
+++ #source/js/script.js
@@ -96,3 +96,18 @@ $(function () {
 		cursorborder: "0px solid #fff",
 	});
 });
+$(function () {
+	$("#scroll-1").niceScroll("#scroll-text-1", {
+		cursorfixedheight: 40,
+		cursorcolor: "#527EDF",
+		cursorwidth: "5px",
+		background: "",
+		autohidemode: false,
+		bouncescroll: false,
+		cursorborderradius: "10px",
+		scrollspeed: 10,
+		mousescrollstep: 10,
+		directionlockdeadzone: 0,
+		cursorborder: "0px solid #fff",
+	});
+});
