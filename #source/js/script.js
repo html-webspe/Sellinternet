198387b825e2f2f87e6d18e5d674c73027765961
hron2017 <andreypbiz@gmail.com>
2020-11-04 20:38:31 +0300

    first commit

--- #source/js/script.js
+++ #source/js/script.js
@@ -80,3 +80,19 @@ counters.forEach(counter => {
 	updateCount();
 });
 //==================== <!-- Price-counter --> ========================//
+
+$(function () {
+	$("#scroll").niceScroll("#scroll-text", {
+		cursorfixedheight: 100,
+		cursorcolor: "#527EDF",
+		cursorwidth: "5px",
+		background: "",
+		autohidemode: false,
+		bouncescroll: false,
+		cursorborderradius: "10px",
+		scrollspeed: 10,
+		mousescrollstep: 10,
+		directionlockdeadzone: 0,
+		cursorborder: "0px solid #fff",
+	});
+});
--- #source/js/vendors.js
+++ #source/js/vendors.js
@@ -1,1 +1,1 @@
-//@prepros-append libs/slick.min.js
+//@prepros-append libs/jquery.nicescroll.min.js
