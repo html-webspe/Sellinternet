2a43f87ace652310a6e0de59137d05adee1302f2
hron2017 <andreypbiz@gmail.com>
2020-11-05 12:04:54 +0300

    comit

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,10 +1,3 @@
 //@prepros-append jq-start.js
-
-//@prepros-append blocks/mobile-menu.js
-//@prepros-append blocks/popup.js
-
-//@prepros-append blocks/forms.js
-
 //@prepros-append script.js
-
 //@prepros-append jq-end.js
